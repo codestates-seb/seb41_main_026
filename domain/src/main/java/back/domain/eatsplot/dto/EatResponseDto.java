1494a906cfe9385b004f1cb98ea4cc4886005820
wondo07 <110904783+wondo07@users.noreply.github.com>
2023-01-18 21:44:31 +0900

    refeat :  EatResponseDto 구현

---
+++ domain/src/main/java/back/domain/eatsplot/dto/EatResponseDto.java
@@ -0,0 +1,16 @@
+package back.domain.eatsplot.dto;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class EatResponseDto {
+    private Long eatId;
+    private String name;
+    private String lat;
+    private String lng;
+}
