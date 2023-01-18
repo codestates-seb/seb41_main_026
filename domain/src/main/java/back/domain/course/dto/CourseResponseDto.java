50ba842ad6bd131df8af54af5086b43543880de7
wondo07 <110904783+wondo07@users.noreply.github.com>
2023-01-18 21:41:47 +0900

    refactoring :  CourseResponseDto 수정(content 삭제)

--- domain/src/main/java/back/domain/course/dto/CourseResponseDto.java
+++ domain/src/main/java/back/domain/course/dto/CourseResponseDto.java
@@ -4,7 +4,10 @@ package back.domain.course.dto;
 import back.domain.comment.entity.Comment;
 //import back.domain.course.entity.CourseLike;
 import back.domain.course.entity.CourseLike;
+import back.domain.coursedata.entity.CourseData;
+import back.domain.eatsplot.entity.Eat;
 import back.domain.sleepspot.entity.Sleep;
+import back.domain.travelspot.entity.TravelSpot;
 import lombok.*;
 
 import java.util.List;
@@ -18,7 +21,7 @@ public class CourseResponseDto {
 
     private Long courseId;
     private String courseName;
-    private String content;
+    private List<CourseData> courseDatas;
     private String tag;
     private String location;
     private int viewCount;
@@ -27,6 +30,7 @@ public class CourseResponseDto {
     private List<Comment> comments;
     private List<CourseLike> courseLikes;
     private List<Sleep> sleeps;
-
+    private List<Eat> eats;
+    private List<TravelSpot> travelSpots;
 
 }
