d8d4e7df7713ca3e3a0ecfda2ba064cf13df307a
wondo07 <110904783+wondo07@users.noreply.github.com>
2023-01-18 21:47:21 +0900

    refeat :  PathPostDto 구현 (경로 설정)

---
+++ domain/src/main/java/back/domain/travelspot/dto/PathPostDto.java
@@ -0,0 +1,19 @@
+package back.domain.travelspot.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PathPostDto {
+
+//    private Long travelSpotId;
+    private List<String> route1;
+    private List<String> route2;
+
+}
