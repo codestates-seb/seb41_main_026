11849f0c10b25e18f4fc0c1d279a7159bbb375b8
wondo07 <110904783+wondo07@users.noreply.github.com>
2023-01-18 21:40:27 +0900

    reat : CourseDataResponseDto 구현

---
+++ domain/src/main/java/back/domain/coursedata/dto/CourseDataResponseDto.java
@@ -0,0 +1,16 @@
+package back.domain.coursedata.dto;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class CourseDataResponseDto {
+
+    private Long courseDataId;
+    private String title;
+    private String text;
+}
