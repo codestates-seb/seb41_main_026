5c02d27f50d685b6c7b49fd02f8357a33f284a7f
wondo07 <110904783+wondo07@users.noreply.github.com>
2023-01-18 21:48:45 +0900

    refeat :  SleepController 구현

--- domain/src/main/java/back/domain/sleepspot/controller/SleepController.java
+++ domain/src/main/java/back/domain/sleepspot/controller/SleepController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/sleep")
