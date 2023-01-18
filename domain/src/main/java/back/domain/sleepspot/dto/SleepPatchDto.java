6bfbd1ca59f2504c5907157f68a9e0192a4683d1
wondo07 <110904783+wondo07@users.noreply.github.com>
2023-01-18 10:10:45 +0900

    refeat : SleepPatchDto 구현

---
+++ domain/src/main/java/back/domain/sleepspot/dto/SleepPatchDto.java
@@ -0,0 +1,16 @@
+package back.domain.sleepspot.dto;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class SleepPatchDto {
+
+    private String Name;
+    private String lat;
+    private String lng;
+}
