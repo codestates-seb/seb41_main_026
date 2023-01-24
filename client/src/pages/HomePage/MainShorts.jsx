b37ada003075c1e70355f4fd4a407c8a65cca119
jingoworld <lonul83@gmail.com>
2023-01-25 00:16:58 +0900

    Feat: 쇼츠 페이지 구현

--- client/src/pages/HomePage/MainShorts.jsx
+++ client/src/pages/HomePage/MainShorts.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Layout from '../../components/Common/Layout';
 
 function MainShorts() {
   return (
-    <div className="container-fluid ps-0 pe-0">
-      <iframe
-        title="taggbox"
-        src="https://widget.taggbox.com/119414"
-        style={{ width: '100%', height: '100vh', border: 'none' }}
-      />
-    </div>
+    <Layout header footer>
+      <div className="container-fluid ps-0 pe-0">
+        <iframe
+          title="taggbox"
+          src="https://widget.taggbox.com/119175"
+          style={{ width: '100%', height: '100vh', border: 'none' }}
+        />
+      </div>
+    </Layout>
   );
 }
 
