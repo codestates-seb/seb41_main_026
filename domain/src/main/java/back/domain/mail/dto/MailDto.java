86a3fb7d363036ef9a4c7c106f273b366860e6a4
Seongmin <bimils7856@gmail.com>
2023-01-26 14:10:37 +0900

    feat: MailService 추가

---
+++ domain/src/main/java/back/domain/mail/dto/MailDto.java
@@ -0,0 +1,37 @@
+package back.domain.mail.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class MailDto {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SendPassword{
+        private String name;
+        private String email;
+        private String title;
+        private String message;
+
+    }
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class AuthEmail{
+        private String email;
+        private String title;
+        private String message;
+        private String createKey;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Key{
+        private String createKey;
+    }
+}
+
