6e4d7794606498502712f8e212ec65d9078decfd
yolodion <diony5338@gmail.com>
2023-01-28 17:12:12 +0900

    Fix: 경기도 Bg 추가

--- client/src/components/RegionSection/RegionSection.jsx
+++ client/src/components/RegionSection/RegionSection.jsx
@@ -80,7 +80,7 @@ const bgLink = [
   { id: 0, region: '서울', imgLink: '/img/seoulBg.jpg' },
   { id: 1, region: '부산', imgLink: '/img/busanBg.jpg' },
   { id: 2, region: '경상도', imgLink: '/img/jirisanBg.jpg' },
-  { id: 3, region: '경기도', imgLink: '/img/geonggidoBg.jgp' },
+  { id: 3, region: '경기도', imgLink: '/img/geonggidoBg.jpg' },
   { id: 4, region: '강원도', imgLink: '/img/naksansaBg.jpg' },
 ];
 
